95821aec39d1ac7e435cbf0a746831cf0845c530
muskanbatra-dev <muskanbatra64@gmail.com>
2021-12-10 08:37:23 +0530

    updateapp.js

---
+++ app.js
@@ -0,0 +1,226 @@
+var readlineSync = require('readline-sync')
+var score = 0;
+
+var questionOne = ("Am i older than 25?");
+var answerone = "no";
+
+
+// input
+var userAnswer = readlineSync.question(questionOne);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerone) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+
+
+var questiontwo = ("Do i like mangoes?");
+var answertwo = "yes";
+
+
+// input
+var userAnswer = readlineSync.question(questiontwo);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answertwo) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionthree = ("Do i like cricket?");
+var answerthree = "yes";
+
+
+// input
+var userAnswer = readlineSync.question(questionthree);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerthree) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionfour = ("Do i live in mumbai?");
+var answerfour = "no";
+
+
+// input
+var userAnswer = readlineSync.question(questionfour);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerfour) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionfive = ("do i like green colour?");
+var answerfive = "no";
+
+
+// input
+var userAnswer = readlineSync.question(questionfive);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerfive) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionsix = ("Do i like photography?");
+var answersix = "yes";
+
+
+// input
+var userAnswer = readlineSync.question(questionsix);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answersix) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionseven = ("am i a good dancer?");
+var answerseven = "no";
+
+
+// input
+var userAnswer = readlineSync.question(questionseven);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerseven) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questioneight = ("is my hieght 6 feet?");
+var answereight = "no";
+
+
+// input
+var userAnswer = readlineSync.question(questioneight);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answereight) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+var questionnine = ("Do i like beaches?");
+var answernine = "yes";
+
+
+// input
+var userAnswer = readlineSync.question(questionnine);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answernine) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
+
+var questionten = ("am i an introvert?");
+var answerten = "no";
+
+
+// input
+var userAnswer = readlineSync.question(questionten);
+
+
+console.log("you entered " + userAnswer)
+
+// processing
+if (userAnswer== answerten) {
+  console.log("you are right")
+
+score = score + 1;
+console.log("score is: "+ score)}
+else {
+  console.log("you are wrong")
+
+score = score - 1;
+console.log("score is"+ score)}
+
